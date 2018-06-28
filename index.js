e1ad137d47b259c3140a7e8b3066aaf75cd06061
Jason Hang <jasonxhang@gmail.com>
2018-06-28 17:45:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello," + " " + specialGuest + "!";
+=======
+var greetSpecialGuest = "Hello" + " " + specialGuest + "!";
+>>>>>>> bcdb0aa7d24591660849617d9e9319d62d47bc2d
 
 var topic = "space";
 
